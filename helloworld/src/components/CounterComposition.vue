ad22986b53b45352f5b8c54df8790410ce837657
Ganne Moreno Pablo Matias <proyectoganne@gmail.com>
2025-10-13 18:16:00 -0300

    limpieza del formato al agregar setup

--- helloworld/src/components/CounterComposition.vue
+++ helloworld/src/components/CounterComposition.vue
@@ -1,18 +1,14 @@
 <template>
-<h3>{{ counter }}</h3>
-<button @click="addCounter">Incrementar</button>
+    <h3>{{ counter }}</h3>
+    <button @click="addCounter">Incrementar</button>
 </template>
 
-<script lang="ts">
-import { defineComponent, Ref, ref } from 'vue';
-export default defineComponent({
-    name:'CounterComposition',
-    setup() {
-        let counter:Ref = ref(0)
-        const addCounter = ():void => {
-            counter.value++
-        }
-        return {counter, addCounter}
-    }
-})
+<script lang="ts" setup>
+import { Ref, ref } from 'vue';
+
+let counter: Ref = ref(0)
+const addCounter = (): void => {
+    counter.value++
+}
+
 </script>

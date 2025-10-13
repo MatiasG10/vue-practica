8dd2b1fdb0b0bd2ba15b687be48a693256d3157d
Ganne Moreno Pablo Matias <proyectoganne@gmail.com>
2025-10-13 15:08:05 -0300

    creacion de componentes

---
+++ helloworld/src/components/PostButton.vue
@@ -0,0 +1,22 @@
+<template>
+    <button class="post-btn">ACCEDER</button>
+</template>
+
+<script lagn="ts" setup>
+
+</script>
+
+<style scoped>
+.post-btn{
+    padding: 16px;
+    background-color: blueviolet;
+    color: white;
+    border: 0;
+    outline: 0;
+}
+
+.post-btn:hover{
+    background-color: white;
+    color: blueviolet;
+}
+</style>

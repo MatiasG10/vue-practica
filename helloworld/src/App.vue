9e49b3effbffd399514f3dcaa0d110890928a72e
Ganne Moreno Pablo Matias <proyectoganne@gmail.com>
2025-10-13 18:00:20 -0300

    pasos con Option API

--- helloworld/src/App.vue
+++ helloworld/src/App.vue
@@ -1,10 +1,9 @@
 <template>
-  <h1>Listado de posts:</h1>
-  <PostContainer></PostContainer>
+  <CounterOption></CounterOption>
 </template>
 
 <script lang="ts" setup>
-import PostContainer from './components/PostContainer.vue';
+import CounterOption from './components/CounterOption.vue';
 </script>
 
 <style>

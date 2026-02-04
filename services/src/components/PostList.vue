56ac41d48c5dd2e2c1f70073114c59c486ec0c70
Ganne Moreno Pablo Matias <proyectoganne@gmail.com>
2026-02-04 16:34:49 -0300

    clase 8
    - Desarrollo del servicio
    - Uso del servicio desde el componente
    - Traducción a Option API y Composition Setup
    - Tipado de datos para TypeScript
    - Estilos

---
+++ services/src/components/PostList.vue
@@ -0,0 +1,75 @@
+<template>
+    <div>Listado de posts</div>
+    <ul class="post-list">
+        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+
+import PostService from '@/services/PostService';
+import { defineComponent, onMounted } from 'vue';
+
+const service = new PostService()
+const posts = service.getPosts()
+
+onMounted(async () => {
+    await service.fetchAll()
+})
+
+
+// import PostService from '@/services/PostService';
+// import { defineComponent, onMounted } from 'vue';
+
+// export default defineComponent({
+//     name: 'PostList',
+//     setup() {
+//        const service = new PostService()
+//        const posts = service.getPosts() 
+
+
+//        onMounted(async () =>{
+//             await service.fetchAll()
+//        })
+
+//        return {
+//         posts
+//        }
+//     }
+
+// });
+
+//option API
+// import PostService from '@/services/PostService';
+// import { defineComponent, onMounted } from 'vue';
+
+// export default defineComponent({
+//     name: 'PostList',
+//     data() {
+//         const service = new PostService()
+//         const posts = service.getPosts()
+//         return {posts, service}
+//     },
+//     async mounted() {
+//         await this.service.fetchAll()
+//     }
+
+// });
+
+
+</script >
+
+<style scope>
+    .post-list {
+        width: 95vw;
+        height: 75px;
+        padding: 20px;
+        list-style-type: none;
+    }
+
+    .post-list li {
+        width: 100%;
+        padding: 10px;
+        border: 1px solid #ccc;
+    }
+</style>
